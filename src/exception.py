59c229b2af27365a7dd39879287e071fa40024ec
krgau020 <krgau020@gmail.com>
2023-09-19 12:00:58 +0530

    logger&Excep

--- src/exception.py
+++ src/exception.py
@@ -20,4 +20,10 @@ class CustomException(Exception):
         return self.error_message
     
 
+if __name__=="__main__":
 
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Division by zero error")
+        raise CustomException(e, sys)
